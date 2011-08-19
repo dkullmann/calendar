0d1aef1a269bb0edd1781d5c4672fdf5559c5793
David Kullmann <kullmann.david@gmail.com>
2011-08-19 15:50:29 -0400

    Added new tests for Event model

--- tests/cases/models/event.test.php
+++ tests/cases/models/event.test.php
@@ -33,6 +33,7 @@ class EventTestCase extends AppTestCase {
 		$this->Event = AppMock::getTestModel('Event');
 		$fixture = new EventFixture();
 		$this->record = array('Event' => $fixture->records[0]);
+		$this->recurringRecord = array('Event' => $fixture->records[2]);
 	}
 
 /**
@@ -60,16 +61,98 @@ class EventTestCase extends AppTestCase {
 		$result = $this->Event->add(99, $data);
 		$this->assertTrue($result);
 		
+		// $event = $this->Event->read();
+
+		$result = $this->Event->add(100, $data, 'weekly');
+		$this->assertTrue($result);
+
+		$id = $this->Event->id;
+		$events = $this->Event->find('all', array(
+			'conditions' => array($this->Event->alias.'.id' => $this->Event->id),
+			'recurs' => array(
+				'from' => date('Y-m-d', strtotime('+10 weeks')),
+				'to' => date('Y-m-d', strtotime('+11 weeks'))
+			)
+		));
+
+		$records = Set::extract(sprintf('/Event[id=/%s/]', $id), $events);
+		$this->assertEqual(count($records), 1);
+
 		try {
 			$data = $this->record;
 			unset($data['Event']['id']);
-			//unset($data['Event']['title']);
+			unset($data['Event']['title']);
 			$result = $this->Event->add(99, $data);
 			$this->fail('No exception');
 		} catch (OutOfBoundsException $e) {
 			$this->pass('Correct exception thrown');
 		}
 		
+	}
+	
+/**
+ * Test localization behavior integration
+ *
+ * @return void
+ * @author David Kullmann
+ */
+	public function testLocalization() {
+		$data = $this->record;
+		$local_time_zone = $data['Event']['time_zone'];
+		unset($data['Event']['id']);
+		
+		LocalizeTime::setUserTimeZone($local_time_zone);
+
+		$result = $this->Event->add(99, $data);
+		$this->assertTrue($result);
+		
+		$this->Event->Behaviors->detach('LocalizeTime');		
+		$event = $this->Event->read();
+		$LclDateTimeZone  = new DateTimeZone($local_time_zone);
+		$UtcDateTimeZone  = new DateTimeZone('UTC');
+		$DateTimeStart = new DateTime($data['Event']['start_date'], $LclDateTimeZone);
+		$DateTimeEnd   = new DateTime($data['Event']['end_date'], $LclDateTimeZone);
+		
+		$DateTimeStart->setTimezone($UtcDateTimeZone);;
+		$DateTimeEnd->setTimezone($UtcDateTimeZone);;
+		
+		$this->assertEqual(
+			$DateTimeStart->format('Y-m-d H:i:s'),
+			$event[$this->Event->alias]['start_date']
+		);
+
+		$this->assertEqual(
+			$DateTimeEnd->format('Y-m-d H:i:s'),
+			$event[$this->Event->alias]['end_date']
+		);
+	}
+/**
+ * Test integration with recurrence behavior
+ *
+ * @return void
+ * @author David Kullmann
+ */
+	public function testRecurrence() {
+		$data = $this->recurringRecord;
+		$results = $this->Event->find('all', array(
+			'recurs' => array(
+				'from' => date('Y-m-d'),
+				'to' => date('Y-m-d', strtotime('+1 weeks')),
+			)
+		));
+		
+		$records = Set::extract(sprintf('/Event[id=/%s/]', $data['Event']['id']), $results);
+		$this->assertEqual(count($records), 1);
+		
+		$results = $this->Event->find('all', array(
+			'recurs' => array(
+				'from' => date('Y-m-d'),
+				'to' => date('Y-m-d', strtotime('+4 weeks')),
+			)
+		));
+		
+		$records = Set::extract(sprintf('/Event[id=/%s/]', $data['Event']['id']), $results);
+		$this->assertEqual(count($records), 4);
 	}
 
 /**
@@ -79,30 +162,48 @@ class EventTestCase extends AppTestCase {
  * @access public
  */
 	public function testEdit() {
-		$result = $this->Event->edit('event-1', null);
-
-		$expected = $this->Event->read(null, 'event-1');
+		$data = $this->record;
+		$id = $data[$this->Event->alias]['id'];
+		$user_id = $this->Event->Calendar->field('user_id',
+			array($this->Event->Calendar->alias.'.id' => $data[$this->Event->alias]['calendar_id'])
+		);
+		$result = $this->Event->edit($id, $user_id, null);
+		
+		$expected = $this->Event->read(null, $id);
 		$this->assertEqual($result['Event'], $expected['Event']);
-
+		
 		// put invalidated data here
-		$data = $this->record;
-		//$data['Event']['title'] = null;
-
-		$result = $this->Event->edit('event-1', $data);
+		$data['Event']['title'] = null;
+		
+		$result = $this->Event->edit($id, $user_id, $data);
 		$this->assertEqual($result, $data);
-
+		
 		$data = $this->record;
-
-		$result = $this->Event->edit('event-1', $data);
+		
+		$result = $this->Event->edit($id, $user_id, $data);
 		$this->assertTrue($result);
-
-		$result = $this->Event->read(null, 'event-1');
-
+		
+		$result = $this->Event->read(null, $id);
+		
 		// put record specific asserts here for example
 		// $this->assertEqual($result['Event']['title'], $data['Event']['title']);
 
+		$result = $this->Event->edit($id, $user_id, $data, 'weekly');
+		$this->assertTrue($result);
+
+		$events = $this->Event->find('all', array(
+			'conditions' => array($this->Event->alias.'.id' => $this->Event->id),
+			'recurs' => array(
+				'from' => date('Y-m-d', strtotime('+10 weeks')),
+				'to' => date('Y-m-d', strtotime('+11 weeks'))
+			)
+		));
+
+		$records = Set::extract(sprintf('/Event[id=/%s/]', $id), $events);
+		$this->assertEqual(count($records), 1);
+	
 		try {
-			$this->Event->edit('wrong_id', $data);
+			$this->Event->edit('wrong_id', $user_id, $data);
 			$this->fail('No exception');
 		} catch (OutOfBoundsException $e) {
 			$this->pass('Correct exception thrown');
@@ -116,9 +217,11 @@ class EventTestCase extends AppTestCase {
  * @access public
  */
 	public function testView() {
-		$result = $this->Event->view('event-1');
+		$data = $this->record;
+		$id = $data[$this->Event->alias]['id'];
+		$result = $this->Event->view($id);
 		$this->assertTrue(isset($result['Event']));
-		$this->assertEqual($result['Event']['id'], 'event-1');
+		$this->assertEqual($result['Event']['id'], $id);
 
 		try {
 			$result = $this->Event->view('wrong_id');
@@ -135,25 +238,30 @@ class EventTestCase extends AppTestCase {
  * @access public
  */
 	public function testValidateAndDelete() {
+		$data = $this->record;
+		$id = $data[$this->Event->alias]['id'];
+		$user_id = $this->Event->Calendar->field('user_id',
+			array($this->Event->Calendar->alias.'.id' => $data[$this->Event->alias]['calendar_id'])
+		);
 		try {
-			$postData = array();
-			$this->Event->validateAndDelete('invalidEventId', $postData);
-		} catch (OutOfBoundsException $e) {
-			$this->assertEqual($e->getMessage(), 'Invalid Event');
-		}
-		try {
-			$postData = array(
-				'Event' => array(
-					'confirm' => 0));
-			$result = $this->Event->validateAndDelete('event-1', $postData);
-		} catch (Exception $e) {
-			$this->assertEqual($e->getMessage(), 'You need to confirm to delete this Event');
-		}
-
+				$postData = array();
+				$this->Event->validateAndDelete('invalidEventId', $postData);
+			} catch (OutOfBoundsException $e) {
+				$this->assertEqual($e->getMessage(), 'Invalid Event');
+			}
+			try {
+				$postData = array(
+					'Event' => array(
+						'confirm' => 0));
+				$result = $this->Event->validateAndDelete($id, $user_id, $postData);
+			} catch (Exception $e) {
+				$this->assertEqual($e->getMessage(), 'You need to confirm to delete this Event');
+			}
+	
 		$postData = array(
 			'Event' => array(
 				'confirm' => 1));
-		$result = $this->Event->validateAndDelete('event-1', $postData);
+		$result = $this->Event->validateAndDelete($id, $user_id, $postData);
 		$this->assertTrue($result);
 	}
 	
